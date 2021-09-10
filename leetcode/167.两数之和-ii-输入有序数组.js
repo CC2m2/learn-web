5953ce348cb9c860977bf82bc2c2507fef182384
CC2m2 <389269570@qq.com>
2021-09-10 23:58:07 +0800

    added leetcode 167.两数之和Ⅱ-输入有序数组

--- leetcode/167.两数之和-ii-输入有序数组.js
+++ leetcode/167.两数之和-ii-输入有序数组.js
@@ -29,5 +29,21 @@ function myTwoSum(numbers, target) {
     }
   }
 }
+
+//循环条件优化
+// const twoSum = (numbers, target) => {
+//   let p1 = 0
+//   let p2 = numbers.length - 1
+  
+//   while (numbers[p1] + numbers[p2] !== target) {
+//       if (numbers[p1] + numbers[p2] > target) {
+//           p2--
+//       } else {
+//           p1++
+//       }
+//   }
+  
+//   return [p1 + 1, p2 + 1]
+//}
 // @lc code=end
 
