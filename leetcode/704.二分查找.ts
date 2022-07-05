b0148d02350c511c159a6e7a6cca9e7a3cd66c0b
CC2m2 <389269570@qq.com>
2022-07-05 23:20:57 +0800

    add 704.二分查找.ts

---
+++ leetcode/704.二分查找.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=typescript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+const nums = [-1,0,3,5,9,12];
+console.log(search(nums, 2));
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (target === nums[middle]) {
+      return middle;
+    }
+
+    if (target < nums[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+
+  return -1;
+};
+// @lc code=end
+
